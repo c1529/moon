fc63f33c1c4e0acb15206ddbdc40171ad0c0b6fe
moony1529 <104741605+moony1529@users.noreply.github.com>
2022-10-26 18:33:32 +0800

    Create 选择排序.cpp

---
+++ cpp/选择排序.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+void sort_1(int a[], int n)   //冒泡
+{
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+void sort_2(int num[], int n)  //选择
+{
+	int i, j, k,min,temp;
+	for (i = 0; i < n-1; i++)
+	{
+		min = i;
+		for (j = i; j < n; j++)
+		{
+			if (num[i] > num[j ])
+			{
+				min = j;
+				temp = num[i];
+				num[i] = num[j];
+				num[j] = temp;
+			}
+		}
+	}
+}
+void print(int num[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		cout << num[i] << endl;
+	}
+}
+int main()
+{
+	int n;
+	int num[5] = { 5,16,3,18,17 };
+	n = sizeof(num) / sizeof(int);
+	sort_2(num, n);
+	print(num, n);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
